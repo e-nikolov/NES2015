968f73c0ee1a0a1f75d00bc9b1f91a96274c5fe2
e-nikolov <emil.e.nikolov@gmail.com>
2015-10-20 16:54:31 +0200

    \n

--- sensor_v_1/sensor.c
+++ sensor_v_1/sensor.c
@@ -176,14 +176,14 @@ static const struct broadcast_callbacks broadcast_callbacks = {recv_broadcast};
 PROCESS_THREAD(sensor_cast_process, ev, data)
 {
 
-	printf("sensor_cast_process: waiting for daddy_addr");
+	printf("sensor_cast_process: waiting for daddy_addr\n");
 	PROCESS_EXITHANDLER(runicast_close(&runicast);)
 	PROCESS_BEGIN();
 
 	broadcast_open(&broadcast, 129, &broadcast_callbacks);
 
 
-	printf("sensor_cast_process: waiting for daddy_addr");
+	printf("sensor_cast_process: waiting for daddy_addr\n");
 	PROCESS_WAIT_UNTIL(daddy_addr != NULL);
 
 	runicast_open(&runicast, 129, &runicast_callbacks);
@@ -193,7 +193,7 @@ PROCESS_THREAD(sensor_cast_process, ev, data)
 	while(1)
 	{
 
-		printf("sensor_cast_process: main loop");
+		printf("sensor_cast_process: main loop\n");
 		static struct etimer et;
 		struct runicast_message msg;
 
